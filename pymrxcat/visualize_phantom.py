32d9bf1d41dea006870ddab09e92868fdea343d3
CyprienBouton <boutoncyprien@yahoo.com>
2025-04-11 18:08:28 +0200

    [up] pymrxcat/visualize_phantom.py

--- pymrxcat/visualize_phantom.py
+++ pymrxcat/visualize_phantom.py
@@ -22,8 +22,8 @@ def display_phantom_data(fname=None, plot_result=True):
     
     # Extract the data from the .npz file
     rho_map = phantom_data['PD_map']
-    t1_maps = np.moveaxis(phantom_data['T1_map'], 0, -1)
-    t2_maps = np.moveaxis(phantom_data['T2_map'], 0, -1)
+    t1_maps = phantom_data['T1_map']
+    t2_maps = phantom_data['T2_map']
     sen = phantom_data['coil_sens']
     
     
@@ -33,28 +33,25 @@ def display_phantom_data(fname=None, plot_result=True):
         mid_slice = rho_map.shape[2]//2
         # Display rho_map (proton density map)
         plt.subplot(2, 2, 1)
-        # plt.imshow(rho_map[..., mid_slice], cmap='gray')
-        display_movie(rho_map, [2,2,1], loops=1, dt=0.2)
+        display_movie(rho_map, [2,2,1], loops_nb=1, dt=0.2, loop_axis=-1)
         plt.title('Rho Map')
         plt.axis('off')
         
         # Display T1 maps (showing one slice)
         plt.subplot(2, 2, 2)
-        # plt.imshow(t1_maps[..., mid_slice], cmap='hot')  # Displaying the first T1 map
-        display_movie(t1_maps[..., mid_slice,:], [2,2,2], loops=1, dt=0.2)
+        display_movie(t1_maps[..., mid_slice], [2,2,2], loops_nb=1, dt=0.2)
         plt.title('T1 Map')
         plt.axis('off')
         
         # Display T2 maps (showing one slice)
         plt.subplot(2, 2, 3)
-        # plt.imshow(t2_maps[..., mid_slice], cmap='hot')  # Displaying the first T2 map
-        display_movie(t2_maps[..., mid_slice,:], [2,2,3], loops=1, dt=0.2)
+        display_movie(t2_maps[..., mid_slice], [2,2,3], loops_nb=1, dt=0.2)
         plt.title('T2 Map')
         plt.axis('off')
         
         # Display sensitivity map (coil-combined image)
         plt.subplot(2, 2, 4)
-        display_movie(np.abs(sen[..., mid_slice, :]), [2,2,4], loops=1, dt=1)
+        display_movie(np.abs(sen[..., mid_slice]), [2,2,4], loops_nb=1, dt=1)
         plt.title('Sensitivity Map (Coil Combined)')
         plt.axis('off')
 
@@ -62,8 +59,9 @@ def display_phantom_data(fname=None, plot_result=True):
 
     return phantom_data, fname
 
-def display_movie(data, subplotno, loops=3, dt=0.1, scale=(None, None)):
-    number_images = data.shape[2]
+def display_movie(data, subplotno, loops_nb=3, dt=0.1, scale=(None, None), loop_axis=0):
+    number_images = data.shape[loop_axis]
+    data = np.moveaxis(data, loop_axis, 0)
     image_pos_no = 1
 
     if len(subplotno) >= 2:
@@ -87,20 +85,32 @@ def display_movie(data, subplotno, loops=3, dt=0.1, scale=(None, None)):
         maxint += 1
         minint -= 1
 
-    for _ in range(loops):
+    for _ in range(loops_nb):
         for imageno in range(number_images):
             plt.subplot(gridsizex, gridsizey, image_pos_no)
-            plt.imshow(data[:, :, imageno], cmap='gray', vmin=minint, vmax=maxint)
+            imshow(data[imageno], cmap='gray', vmin=minint, vmax=maxint)
             plt.title(f'{imageno + 1}/{number_images}')
             plt.axis('off')
             plt.pause(dt)
 
     imageno = number_images // 2
     plt.subplot(gridsizex, gridsizey, image_pos_no)
-    plt.imshow(data[:, :, imageno], cmap='gray', vmin=minint, vmax=maxint)
+    imshow(data[imageno], cmap='gray', vmin=minint, vmax=maxint)
     plt.title(f'{imageno + 1}/{number_images}')
     plt.axis('off')
 
+
+def imshow(data, *args, **kwargs):
+    """ Display an image using Matplotlib's `imshow`, with the orientation adjusted
+    to match the MATLAB-style image conventions (i.e., flipping the vertical axis).
+    Same function use in MRzeroCore.util
+
+    Args:
+        data (np.ndarray): input image
+        *args: Additional positional arguments passed to `plt.imshow`.
+        **kwargs: Additional keyword arguments passed to `plt.imshow`.
+    """
+    plt.imshow(data.T, origin='lower', *args, **kwargs)
 
 def main():
     display_phantom_data()
