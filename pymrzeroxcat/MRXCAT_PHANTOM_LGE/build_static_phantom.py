78fd6e63dfb6df348c0b465a33b0d54a11359e2e
CyprienBouton <boutoncyprien@yahoo.com>
2025-09-01 09:10:52 +0200

    [up] standardize and organize build_static

--- pymrzeroxcat/MRXCAT_PHANTOM_LGE/build_static_phantom.py
+++ pymrzeroxcat/MRXCAT_PHANTOM_LGE/build_static_phantom.py
@@ -1,119 +1,91 @@
-import os
-import numpy as np
-from scipy.ndimage import zoom
-import json
-from mrtwin import b0field, b1field, sensmap
 import argparse
-from ast import literal_eval
+import json
+import importlib.resources as pkg_resources
 
-from pymrzeroxcat.read_mrxcat_raw_data import get_tissues_id, get_resolution, get_segmentation, resample_segmentation, crop_segmentation
-from pymrzeroxcat.read_mrxcat_raw_data import resolve_log_file
+import numpy as np
+import MRzeroCore as mr0
+from mrtwin import b1field, b0field, sensmap
 
+import tkinter as tk
+from tkinter import filedialog
 
+from pymrzeroxcat.convert_arguments import parse_key_value_or_json_file, complete_imaging_args
+from pymrzeroxcat.read_mrxcat_raw_data import get_crop_segmentation_resampled, resolve_log_file, get_tissues_id
+
+JSON_PARAMETERS = pkg_resources.files("pymrzeroxcat").joinpath("../MRXCAT_raw_data/tissues.json").as_posix()
 DEFAULT_T1 = 900    # ms (muscle, organs)
 DEFAULT_T2 = 50     # ms (muscle, soft tissue)
 DEFAULT_T2dash = 30 # ms (typical T2' value for soft tissue)
 DEFAULT_RHO = 85.0    # Between muscle (80) and liver (90)
 DEFAULT_CHI = -9.0    # Typical soft tissue susceptibility (ppm)
-DEFAULT_tissues_param_json = 'MRXCAT_raw_data/tissues.json'
+DEFAULT_D = 0.0       # Typical diffusion coefficient (mm^2/s)
 
 
-def parse_key_value(arg):
-    try:
-        key, value = arg.split('=')
-        return key, literal_eval(value)
-    except ValueError:
-        raise argparse.ArgumentTypeError(f"Arguments must be in key=value format: got '{arg}'")
-
-
-def compute_parameters_maps(bin_file, log_file, bbox=np.array([[0.,1.]]*3), new_resolution=None, tissues_param_json=DEFAULT_tissues_param_json, ):
-    """
-    Compute the T1, T2, T2dash, rho, and chi maps from the binary file.
-    """
-    tissues_parameters = json.load(open(tissues_param_json, 'r'))  # Load tissue parameters from JSON file
-    segmentation = get_segmentation(bin_file, log_file)
-    resolution = get_resolution(log_file)
-    if new_resolution is not None:
-        segmentation = resample_segmentation(segmentation, resolution, new_resolution)
-        resolution = np.array(new_resolution)  # Update resolution to new resolution
-    segmentation = crop_segmentation(segmentation, bbox)
-    FOV = segmentation.shape * resolution
-    print(f"Resolution: {resolution[0]} x {resolution[1]} x {resolution[2]} mm/pixel")
-    print(f"  FOV : {FOV[0]} x {FOV[1]} x {FOV[2]} mm³")
-    print(f"Matrix: {segmentation.shape}")
-
-    default_values = {
-        'T1': DEFAULT_T1,  # ms
-        'T2': DEFAULT_T2,     # ms
-        'T2dash': DEFAULT_T2dash,  # ms
-        'rho': DEFAULT_RHO,   # arbitrary units
-        'chi': DEFAULT_CHI,     # ppm
-    }
-    
-    # initialize parameters
-    t1_map = np.zeros(segmentation.shape, dtype=np.float32)
-    t2_map = np.zeros(segmentation.shape, dtype=np.float32)
-    t2dash_map = np.zeros(segmentation.shape, dtype=np.float32)
-    rho_map = np.zeros(segmentation.shape, dtype=np.float32)
-    chi_map = np.zeros(segmentation.shape, dtype=np.float32)
-    
-    tissues_ids = get_tissues_id(log_file)
-    for tissue_id in tissues_ids:
-        if str(tissue_id) in tissues_parameters:
-            tissue_params = tissues_parameters[str(tissue_id)]
-            t1_map[segmentation == tissue_id] = tissue_params['T1']
-            t2_map[segmentation == tissue_id] = tissue_params['T2']
-            rho_map[segmentation == tissue_id] = tissue_params['rho']
-            chi_map[segmentation == tissue_id] = tissue_params['chi']
-            if 'T2dash' in tissue_params:
-                t2dash_map[segmentation == tissue_id] = tissue_params['T2dash']
-            else:
-                t2dash_map[segmentation == tissue_id] = default_values['T2dash'] # Default value if not specified in tissue parameters
-        # if tissue_id not in tissues_parameters assign default values
-        else:
-            t1_map[segmentation == tissue_id] = default_values['T1']
-            t2_map[segmentation == tissue_id] = default_values['T2']
-            rho_map[segmentation == tissue_id] = default_values['rho']
-            chi_map[segmentation == tissue_id] = default_values['chi']
-            t2dash_map[segmentation == tissue_id] = default_values['T2dash']
-
-    return t1_map, t2_map, t2dash_map, rho_map, chi_map
-
-
-def build_static_phantom(
-    bin_file, 
+def get_phantom_values(
+    bin_file,
     log_file,
-    phantom_file='MRXCAT_phantom.npz', 
-    field_strength=1.5, 
-    plot=True, 
-    bbox=np.array([[0., 1.]]*3), 
-    resolution=None,
+    FOV=(200, 200, 200),
+    matrix=(128,128,5),
+    center_segmentation=[0.4, 0.8, 0.5],
+    field_strength=1.5,
     ncoils=8,
-    tissues_param_json=DEFAULT_tissues_param_json,
-    b0field_kwargs={}, 
+    param_json=JSON_PARAMETERS,
+    b0field_kwargs={},
     b1field_kwargs={},
     sensmap_kwargs={}, 
-    plot_kwargs={}, 
+    phantom_file='T1MES_default.npz',
+    plot=True, 
+    plot_kwargs={},
 ):
     """
-    Build a static phantom for LGE MRI.
-    Parameters:
-        bin_file (str): Path to the binary file containing the phantom data.
-        log_file (str): Path to the log file containing the key 'array_size'.
-        phantom_file (str): Output file path for the generated phantom.
-        field_strength (float): Magnetic field strength in Tesla.
-        plot (bool): Whether to plot the phantom after generation.
-        bbox (np.ndarray): Bounding box for the phantom in the format [[x_min, x_max], [y_min, y_max], [z_min, z_max]].
-        resolution (tuple): Resolution of the phantom in mm/pixel (Nx, Ny, Nz).
-        ncoils (int): Number of coils for the sensitivity map.
-        tissues_param_json (str): Path to a JSON file containing tissue parameters like T1, T2, T2dash, rho, and chi. Default to DEFAULT_tissues_param_json
-        b0field_kwargs (dict): Keyword arguments for the B0 field computation.
-        b1field_kwargs (dict): Keyword arguments for the B1 field computation.
-        sensmap_kwargs (dict): Keyword arguments for the sensitivity map computation
-        plot_kwargs (dict): Keyword arguments for the plot_dynamic
+    Create and save a T1MES phantom using values from doi:10.1186/s12968-016-0280-z.
+
+    Args:
+        bin_file (str): Path to the binary segmentation file.
+        log_file (str): Path to the log file containing the 'array_size' key.
+        FOV (list, tuple): field of view of the phantom [mm].
+        matrix (list, tuple): Resolution matrix.
+        center_segmentation (list, tuple): Center of the segmentation [x, y, z] (relative to FOV).
+        field_strength (float): Field strength in Tesla. Only 1.5 or 3.0 supported.
+        ncoils (int): Number of receiver coils for sensitivity map generation. Defaults to 8.
+        param_json (str): Path to JSON file with phantom parameters. Defaults to 't1mes/T1MES_values.json'.
+        b0field_kwargs (dict, optional): Keyword arguments passed to `mrtwin.b0field()`.
+        b1field_kwargs (dict, optional): Keyword arguments passed to `mrtwin.b1field()`.
+        sensmap_kwargs (dict, optional): Keyword arguments passed to `mrtwin.sensmap()`.
+        phantom_file (str): Path to save the generated phantom .npz file. Defaults to 'T1MES_default.npz'.
+        plot (bool): If True, displays the phantom using MRzeroCore. Defaults to True.
+        plot_kwargs (dict, optional): Keyword arguments passed to `mr0.DynamicVoxelPhantom.plot()`.
     """
-    t1_map, t2_map, t2dash_map, rho_map, chi_map = compute_parameters_maps(bin_file, log_file, bbox, resolution, tissues_param_json=tissues_param_json)
-    
+    # Load T1MES phantom tissue parameters from JSON
+    phantom_data = json.load(open(param_json, 'r'))  # Values from Bruker minispec (1.4 T, 22°C)
+
+    seg = get_crop_segmentation_resampled(bin_file, log_file, FOV, matrix, center_segmentation)
+    tissues_ID = get_tissues_id(log_file)
+    # Initialize property maps
+    T1_map = np.zeros_like(seg, dtype=float)
+    T2_map = np.zeros_like(seg, dtype=float)
+    PD_map = np.zeros_like(seg, dtype=float)
+    chi_map = np.zeros_like(seg, dtype=float)
+    D_map = np.zeros_like(seg, dtype=float)
+    T2dash_map = np.zeros_like(seg, dtype=float)
+
+    # Fill values for each tissue
+    for tissue_id in tissues_ID:
+        if str(tissue_id) not in phantom_data:
+            T1_map[seg == tissue_id] = DEFAULT_T1
+            T2_map[seg == tissue_id] = DEFAULT_T2
+            T2dash_map[seg == tissue_id] = DEFAULT_T2dash
+            PD_map[seg == tissue_id] = DEFAULT_RHO
+            chi_map[seg == tissue_id] = DEFAULT_CHI
+            D_map[seg == tissue_id] = DEFAULT_D
+        else:
+            T1_map[seg == tissue_id] = phantom_data[str(tissue_id)]['T1']
+            T2_map[seg == tissue_id] = phantom_data[str(tissue_id)]['T2']
+            T2dash_map[seg == tissue_id] = phantom_data[str(tissue_id)]['T2dash']
+            PD_map[seg == tissue_id] = phantom_data[str(tissue_id)]['PD']
+            chi_map[seg == tissue_id] = phantom_data[str(tissue_id)]['chi']
+            D_map[seg == tissue_id] = phantom_data[str(tissue_id)]['D']
+
     # compute B0
     b0field_kwargs['chi'] = chi_map
     b0field_kwargs['B0'] = field_strength
@@ -121,36 +93,38 @@ def build_static_phantom(
         b0field_kwargs['b0range'] = (-200,200) # Default range of -200/+200Hz
     B0_map = b0field(**b0field_kwargs)
     
-    # compute B1
+    # Compute B1
     b1field_kwargs['shape'] = B0_map.squeeze().shape
-    b1field_kwargs['mask'] = rho_map.squeeze()>0
-    if 'b1range' not in b1field_kwargs:
-        b1field_kwargs['b1range'] = (.99, 1.)
+    b1field_kwargs['mask'] = PD_map.squeeze()>0
     B1_map = b1field(**b1field_kwargs)
     if B1_map.ndim==2:
         B1_map = np.expand_dims(B1_map, axis=2)
     
     # Compute sensitivity map
-    sensmap_kwargs['shape'] = (ncoils,) + rho_map.shape
+    sensmap_kwargs['shape'] = (ncoils,) + PD_map.shape
     coil_sens = sensmap(**sensmap_kwargs)
-
+    
+    # Add tissue masks
+    tissues_mask = {phantom_data[tissue_id]['description']: seg==int(tissue_id)  for tissue_id in phantom_data.keys()}
+    tissues_mask = {'tissue_'+k: v for k,v in tissues_mask.items()}
+    
+    # Save maps to compressed .npz file (convert T1, T2 to seconds)
     np.savez_compressed(
         phantom_file,
-        PD_map=rho_map,
-        T1_map=t1_map * 1e-3,
-        T2_map=t2_map * 1e-3,
-        T2dash_map=t2dash_map * 1e-3,
-        D_map=np.zeros_like(rho_map),  # Diffusion is set to 0 everywhere
+        PD_map=PD_map,
+        T1_map=T1_map * 1e-3,
+        T2_map=T2_map * 1e-3,
+        T2dash_map=T2dash_map,
+        D_map=D_map,  # Diffusion is set to 0 everywhere
         B0_map=B0_map,
         B1_map=B1_map,
-        FOV=(0.2, 0.2, 0.2),
+        FOV=np.array(FOV, dtype=float)*1e-3, # in meters
         coil_sens=coil_sens,
+        **tissues_mask,
     )
-    print(f"Static phantom saved to {phantom_file}")
-    
-    # Optionally plot the phantom
+
+    # Optional plot: display center slice using MRzeroCore
     if plot:
-        import MRzeroCore as mr0
         phantom = mr0.DynamicVoxelPhantom.load(phantom_file)
         if 'time_unit' not in plot_kwargs:
             plot_kwargs['time_unit'] = 'ms'
@@ -160,57 +134,74 @@ def build_static_phantom(
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Build MRXCAT Phantom from .bin file')
+    parser = argparse.ArgumentParser(description="Build Static T1MES Phantom")
+    # Binary segmentation file
+    binary_group = parser.add_argument_group("Binary Segmentation File")
+    binary_group.add_argument('--bin_file', help='Input binary (.bin) file for phantom generation', default=None)
+    binary_group.add_argument('--log_file', help='Input log (_log) file', default=None)
     
-    parser.add_argument('bin_file', help='Input binary (.bin) file for phantom generation')
-    parser.add_argument('--log_file', help='Input log (_log) file', default=None)
-    parser.add_argument('-p', '--phantom_file', help='Output phantom file (.npz)', default='MRXCAT_phantom.npz')
-    parser.add_argument('-B0', '--field_strength', help='Field strength in Tesla (default: 1.5)', type=float, default=1.5)
-    parser.add_argument('--plot', help='Whether to plot the phantom (default: True)', action=argparse.BooleanOptionalAction, default=True)
-    
-    parser.add_argument('--bbox', help="Bounding box (3x2 array), default: [0.2, 0.7, 0.6, 0.9, 0., 1.]", type=float, nargs='+', default=[0.2, 0.75, 0.55, 0.95, 0., 1.])
-    parser.add_argument('-r', '--resolution', help='Resolution of the phantom (Nx, Ny, Nz)', type=int, nargs=3)
-    
-    parser.add_argument('--ncoils', help='Number of coils for sensitivity map', type=int, default=8)
-    parser.add_argument('--param_json', help="Tissues parameters file (.json)", default=DEFAULT_tissues_param_json)
-        
-    parser.add_argument('--b0_kwargs', help="Keyword arguments passed to `mrtwin.b0field()`.", type=parse_key_value, default={}, nargs='+')
-    parser.add_argument('--b1_kwargs', help="Keyword arguments passed to `mrtwin.b1field()`.", type=parse_key_value, default={}, nargs='+')
-    parser.add_argument('--sensmap_kwargs', help="Keyword arguments passed to `mrtwin.sensmap()`.", type=parse_key_value, default={}, nargs='+')
-    parser.add_argument('--plot_kwargs', help="Keyword arguments passed to `DynamicVoxelPhantom.plot_dynamic()`.", type=parse_key_value, default={}, nargs='+')
+    # Imaging Parameters
+    imaging_group = parser.add_argument_group("Imaging Parameters")
+    imaging_group.add_argument('-FOV', type=float, nargs=3, help='Field of view in mm. Default to (200,200,50)')   
+    imaging_group.add_argument('-r', '--resolution', type=float, nargs=3, help='Voxel resolution. Default to 1x1x5 mm')
+    imaging_group.add_argument('-m', '--matrix', type=int, nargs=3, help='Matrix size. Default to 200x200x10')
+    imaging_group.add_argument('-B0', '--field_strength', type=float, help='Main magnetic field strength in Tesla. Default to 1.5', default=1.5)
+    imaging_group.add_argument('--ncoils', type=int, help='Number of receiver coils. Default to 8', default=8)
+
+    # Phantom Configuration
+    phantom_group = parser.add_argument_group("Phantom Configuration")
+    phantom_group.add_argument('--param_json', type=str, help='Path to JSON file used to generate phantom', default=JSON_PARAMETERS)
+    phantom_group.add_argument('--b0_kwargs', type=parse_key_value_or_json_file, nargs='+', help='JSON string or file path for B0 simulation params', default={})
+    phantom_group.add_argument('--b1_kwargs', type=parse_key_value_or_json_file, nargs='+', help='JSON string or file path for B1 simulation params', default={})
+    phantom_group.add_argument('--sensmap_kwargs', type=parse_key_value_or_json_file, nargs='+', help='JSON string or file path for sensitivity map params', default={})
+
+    # Output
+    output_group = parser.add_argument_group("Output")
+    output_group.add_argument('-p', '--phantom_file', type=str, help='Path to output phantom file', default='T1MES_default.npz')
+
+    # Visualization
+    vis_group = parser.add_argument_group("Visualization")
+    vis_group.add_argument('--plot', action=argparse.BooleanOptionalAction, help="Whether to plot resulting phantom. Default to True", default=True)
+    vis_group.add_argument('--plot_kwargs', type=parse_key_value_or_json_file, nargs='+', help='JSON string or file path with plot customization options', default={})
 
     args = parser.parse_args()
+    
+    args = complete_imaging_args(args)
+    
+    print(f"FOV: {args.FOV}")
+    print(f"Resolution: {args.resolution}")
+    print(f"Matrix: {args.matrix}")
 
-    # Process bbox
-    bbox = np.array(args.bbox).reshape((3, 2))
-    
-    # Convert key-value pairs to dictionaries
-    b0_kwargs = dict(args.b0_kwargs)
-    b1_kwargs = dict(args.b1_kwargs)
-    sensmap_kwargs = dict(args.sensmap_kwargs)
-    plot_kwargs = dict(args.plot_kwargs)
-    
+    for attr in ['b0_kwargs', 'b1_kwargs', 'sensmap_kwargs', 'plot_kwargs']:
+        val = getattr(args, attr)
+        if val:
+            setattr(args, attr, {k: v for d in val for k, v in d.items()})
+
+    if args.bin_file is None:
+        root = tk.Tk()
+        root.withdraw()
+
+        args.bin_file = filedialog.askopenfilename()
+        
     if args.log_file is None:
-        log_file = resolve_log_file(args.bin_file)
-    else:
-        log_file = args.log_file
-    
-    # Call your phantom generator
-    build_static_phantom(
+        args.log_file = resolve_log_file(args.bin_file)
+        
+    get_phantom_values(
         bin_file=args.bin_file,
-        log_file=log_file,
-        phantom_file=args.phantom_file,
+        log_file=args.log_file,
+        FOV=args.FOV,
+        matrix=args.matrix,
         field_strength=args.field_strength,
-        plot=args.plot,
-        bbox=bbox,
-        resolution=args.resolution,
         ncoils=args.ncoils,
-        tissues_param_json=args.param_json,
-        b0field_kwargs=b0_kwargs,
-        b1field_kwargs=b1_kwargs,
-        sensmap_kwargs=sensmap_kwargs,
-        plot_kwargs=plot_kwargs,
+        param_json=args.param_json,
+        b0field_kwargs=args.b0_kwargs,
+        b1field_kwargs=args.b1_kwargs,
+        sensmap_kwargs=args.sensmap_kwargs,
+        phantom_file=args.phantom_file,
+        plot=args.plot,
+        plot_kwargs=args.plot_kwargs,
     )
+
 
 if __name__ == "__main__":
     main()
